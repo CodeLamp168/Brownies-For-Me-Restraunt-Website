ab6967e2555abe05d8fccfe51d0844f7f0c5f7b4
Caleb D. Sim <dylansim168@gmail.com>
2023-10-17 10:04:39 +0800

    about-section

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const mainNavBtn = document.getElementById('nav-btn')
+const mainNavExitBtn = document.getElementById('exit-nav-btn')
 const mainNavDisplay = document.getElementById('nav-display')
 
 
 mainNavDisplay.style.transform = 'translateX(500px)'
-
 mainNavBtn.addEventListener('click', (e) => {
     mainNavDisplay.style.transform = 'translateX(0px)';
+})
+
+mainNavExitBtn.addEventListener('click', (e) => {
+    mainNavDisplay.style.transform = 'translateX(500px)';
 })
